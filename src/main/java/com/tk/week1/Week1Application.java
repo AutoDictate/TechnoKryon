aa1e0bf63e2b2c186c041b405eeef03db406b8f7
AutoDictate <jayasurya0206@gmail.com>
2024-02-19 22:22:09 +0530

    Hello world Implemented

---
+++ src/main/java/com/tk/week1/Week1Application.java
@@ -0,0 +1,18 @@
+package com.tk.week1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1Application.class, args);
+	}
+
+	@Override
+	public void run(String[] args) throws Exception {
+		System.out.println("Hello World !!!");
+	}
+}
